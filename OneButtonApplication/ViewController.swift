7946a6ce438b61ffe69c50dfde0f6ebd9bde893a
ハトネコエ <hato.nekoe@gmail.com>
2016-05-06 02:36:18 +0900

    Initial Commit

---
+++ OneButtonApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneButtonApplication
+//
+//  Created by radio7954 on 5/6/16.
+//  Copyright © 2016 NYAN39. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
